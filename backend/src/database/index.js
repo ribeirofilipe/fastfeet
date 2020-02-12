5f0307a0552e423ea18f82d2af67e8ddbb577a73
ribeiro.filipe94@gmail.com <ribeiro.filipe94@gmail.com>
2020-02-11 23:53:15 -0300

    first commit

---
+++ backend/src/database/index.js
@@ -0,0 +1,31 @@
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+import databaseConfig from '../config/database';
+import User from '../app/models/User';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+    this.mongo();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+      this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      });
+  }
+}
+
+export default new Database();
---
+++ backend/src/routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
+
+const routes = new Router();
+
+routes.post('/session', SessionController.store);
+
+export default routes;
